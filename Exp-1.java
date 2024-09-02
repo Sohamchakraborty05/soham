cdb074454d3692505848e07b1da228c93469e8db
SOHAM CHAKRABORTY <rntc0822004@nttf.co.in>
2024-09-02 13:44:28 +0530

    done

--- Exp-1.java
+++ Exp-1.java
@@ -1,7 +1,7 @@
 public class Exp-1 {
 public static void main(String args[])
 {
-System.out.println("First program in GitHub");
+System.out.println("First programs in GitHub");
 int x= 18;
 String name= Soham;
 System.out.print("My good name is "+ name);
