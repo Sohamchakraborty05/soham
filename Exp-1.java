7dc4e02773bd7984abac52b8bee5aed4d55b7f60
SOHAM CHAKRABORTY <rntc0822004@nttf.co.in>
2024-09-02 12:07:09 +0530

    aa

--- Exp-1.java
+++ Exp-1.java
@@ -4,7 +4,7 @@ public static void main(String args[])
 System.out.println("First program in GitHub");
 int x= 18;
 String name= Soham;
-System.out.print("My name is "+ name);
+System.out.print("My good name is "+ name);
 System.out.print("I am "+ x);
 }
 }
