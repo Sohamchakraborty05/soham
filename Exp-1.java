d55217d66329a540bd6910fe7839922a4b8e797e
SOHAM CHAKRABORTY <rntc0822004@nttf.co.in>
2024-09-27 10:58:07 +0530

    Changes done

--- Exp-1.java
+++ Exp-1.java
@@ -1,10 +1,10 @@
 public class Exp-1 {
 public static void main(String args[])
 {
-System.out.println("First programs in GitHub"); // Its a comments
+System.out.println("First programs in Git"); 
 int x= 18;
 String name= Soham;
-System.out.print("My first name is "+ name);
+System.out.print("My last name is "+ name);
 System.out.print("I am "+ x);
 }
 }
