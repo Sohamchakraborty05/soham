ea4974e048753ba524eae58dbad4e6eaf06d5403
Sohamchakraborty05 <RNTC0822004@nttf.co.in>
2024-09-27 10:52:43 +0530

    Changes has Done! (#2)

--- Exp-1.java
+++ Exp-1.java
@@ -1,10 +1,10 @@
 public class Exp-1 {
 public static void main(String args[])
 {
-System.out.println("First programs in GitHub");
+System.out.println("First programs in GitHub"); // Its a comments
 int x= 18;
 String name= Soham;
-System.out.print("My good name is "+ name);
+System.out.print("My first name is "+ name);
 System.out.print("I am "+ x);
 }
 }
